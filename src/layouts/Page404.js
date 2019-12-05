2f09aeb6915cf0aa1b1be72f501dec04045ca9d3
Scriven Anthony <anthony.scriven@etu.univ-lyon1.fr>
2019-12-05 22:18:13 +0100

    Creation of the 404 error page.

--- src/layouts/Page404.js
+++ src/layouts/Page404.js
@@ -1,15 +1,24 @@
-import { Container } from '@material-ui/core';
+import {Container, makeStyles, Typography} from '@material-ui/core';
 import React from 'react';
 import routes from "../routes";
 
-
+const useStyles = makeStyles(theme => ({
+    root: {
+        textAlign: 'center'
+    }
+}));
 
 export default function Page404({data}) {
-
+    const classes = useStyles();
     return (
-      routes.findIndex(element => element.path === data.location.pathname) < 0 ?
-    <Container maxWidth="lg">
-        Page404
-    </Container> : null
-  )
+        routes.findIndex(element => element.path === data.location.pathname) < 0 ?
+            <Container maxWidth="lg">
+                <Typography variant="h1" component="h2" className={classes.root}>
+                    404 Not Found
+                </Typography>
+                <canvas>
+
+                </canvas>
+            </Container> : null
+    )
 }
