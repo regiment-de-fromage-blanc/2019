bd140adf9bdeb529727575df6f6ca2519c091476
Thomas Peyrot <thomas.peyrot@etu.univ-lyon1.fr>
2019-12-06 02:42:56 +0100

    Got MongoDB connexion

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3001;
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'nuit';
+const MongoClient = require('mongodb').MongoClient(dbUrl, { useUnifiedTopology: true });
+const assert = require('assert');
+
+MongoClient.connect( (err, client) => {
+    // Get DB connexion
+    assert.equal(null, err);
+    console.log("Connected successfully to MongoDB server");
+    const db = client.db(dbName);
+
+    // Routing
+    app.get('/', (req, res) => res.send('Hello World!'));
+});
 
 app.use(cors());
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
